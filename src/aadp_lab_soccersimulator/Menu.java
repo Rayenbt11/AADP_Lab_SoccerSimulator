b4f5997bc8625ea4c488f6c6c35584a6ef48624e
Rayenbt11 <100359918+Rayenbt11@users.noreply.github.com>
2023-12-15 13:22:50 +0000

    finish menu class

--- src/aadp_lab_soccersimulator/Menu.java
+++ src/aadp_lab_soccersimulator/Menu.java
@@ -198,7 +198,40 @@ public class Menu {
     }
 
     private void simulateMatches() {
-        // Logic for option 3: Simulate a number of matches.
-        // This does not interact with the database.
+        System.out.println("How many matches would you like to simulate?");
+        Boolean validNum = false;
+        int numMatches = 0;
+        do {
+            try {
+                numMatches = Integer.parseInt(sc.nextLine());
+                if (numMatches < 1) {
+                    System.out.println("Please enter a positive integer");
+                } else {
+                    validNum = true;
+                }
+
+            } catch (Exception e) {
+                System.out.println("That is not a number. please try again!");
+            }
+        } while (!validNum);
+        for (int matchNum = 1; matchNum <= numMatches; matchNum++) {
+            int team1Num = (int) (Math.floor(Math.random() * teams.length));
+            String team1 = teams[team1Num];
+            int team2Num;
+            do {
+                team2Num = (int) (Math.floor(Math.random() * teams.length));
+            } while (team1Num == team2Num);
+            String team2 = teams[team2Num];
+            int team1Score = (int) (Math.floor(Math.random() * 6));
+            int team2Score = (int) (Math.floor(Math.random() * 6));
+            System.out.println("Time for mathch: " + matchNum);
+            if (team1Score > team2Score) {
+                System.out.println(String.format("Congratulation %s! %s scored %d goals and %s scored %d goals.", team1, team1, team1Score, team2, team2Score));
+            } else if (team1Score < team2Score) {
+                System.out.println(String.format("Congratulation %s! %s scored %d goals and %s scored %d goals.", team2, team1, team1Score, team2, team2Score));
+            } else {
+                System.out.println(String.format("It was a draw!! %s scored %d goals and %s scored %d goals.", team1, team1Score, team2, team2Score));
+            }
+        }
     }
 }
