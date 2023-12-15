296042dd0df664295939c8a6c7ac9ebb02a2ac82
Rayenbt11 <100359918+Rayenbt11@users.noreply.github.com>
2023-12-15 13:06:20 +0000

    adjust main class with database

--- src/aadp_lab_soccersimulator/AADP_Lab_SoccerSimulator.java
+++ src/aadp_lab_soccersimulator/AADP_Lab_SoccerSimulator.java
@@ -13,30 +13,24 @@ import java.util.Scanner;
 
 /**
  *
- * @author Sam
- * Please put the names and student numbers of the team here:
- * 
- * Name 1:
- * Number 1:
- * 
- * Name 2:
- * Number 2:
- * 
- * Name 3:
- * Number 3:
- * 
- * Name 4:
- * Number 4
- * 
+ * @author Sam Please put the names and student numbers of the team here:
+ *
+ * Name 1: Rayen Bentemessek Number 1: 2021378
+ *
+ * Name 2: Number 2:
+ *
+ * Name 3: Number 3:
+ *
+ * Name 4: Number 4
+ *
  */
-
 public class AADP_Lab_SoccerSimulator {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        String dbName = "world_cup";
+        /* String dbName = "world_cup";
         String[] teams = {"Ireland", "Brazil", "Argentina", "Japan", "Mexico", "Senegal", "Tunisia", "Qatar"};
         String DB_URL = "jdbc:mysql://localhost/" + dbName;
         String USER = "football";
@@ -60,7 +54,10 @@ public class AADP_Lab_SoccerSimulator {
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }
+        }*/
+        String[] teams = {"Ireland", "Brazil", "Argentina", "Japan", "Mexico", "Senegal", "Tunisia", "Qatar"};
+        Database.setupDatabase(teams);
+
         int option;
         boolean exit = false;
         Scanner sc = new Scanner(System.in);
@@ -69,7 +66,7 @@ public class AADP_Lab_SoccerSimulator {
             System.out.println("1. Enter a new player to a team.");
             System.out.println("2. See the players on a team.");
             System.out.println("3. Simulate a number of matches.");
-            System.out.println("4. Exit the programme.");            
+            System.out.println("4. Exit the programme.");
             try {
                 option = Integer.parseInt(sc.nextLine());
                 if (option == 1) {
@@ -85,8 +82,12 @@ public class AADP_Lab_SoccerSimulator {
                                 break;
                             }
                         }
-                        if (teamName.toLowerCase().equals("exit")) break;
-                        if (!validTeam) System.out.println("That is not one of the teams. Please try again!");
+                        if (teamName.toLowerCase().equals("exit")) {
+                            break;
+                        }
+                        if (!validTeam) {
+                            System.out.println("That is not one of the teams. Please try again!");
+                        }
                     } while (!validTeam);
                     String name;
                     int number = 0;
@@ -100,35 +101,39 @@ public class AADP_Lab_SoccerSimulator {
                     System.out.println("Please enter the player's number: ");
                     do {
                         try {
-                            number = Integer.parseInt(sc.nextLine()); 
+                            number = Integer.parseInt(sc.nextLine());
                             if (number < 1) {
                                 System.out.println("Please enter a positive integer");
-                            } else validPlayer = true;
+                            } else {
+                                validPlayer = true;
+                            }
 
                         } catch (Exception e) {
                             System.out.println("That is not a number. please try again!");
                         }
-                    } while (!validPlayer);                                              
+                    } while (!validPlayer);
                     System.out.println("Please enter the player's date of birth: ");
-                    birth = sc.nextLine();         
+                    birth = sc.nextLine();
                     System.out.println("Please enter the player's position: ");
-                    position = sc.nextLine(); 
+                    position = sc.nextLine();
                     System.out.println("Please enter the number of goals the player has scored: ");
                     validPlayer = false;
                     do {
                         try {
-                            goalsScored = Integer.parseInt(sc.nextLine()); 
+                            goalsScored = Integer.parseInt(sc.nextLine());
                             if (goalsScored < 1) {
                                 System.out.println("Please enter a positive integer");
-                            } else validPlayer = true;
+                            } else {
+                                validPlayer = true;
+                            }
 
                         } catch (Exception e) {
                             System.out.println("That is not a number. please try again!");
                         }
-                    } while (!validPlayer);     
+                    } while (!validPlayer);
                     System.out.println("Please enter the player's background: ");
-                    background = sc.nextLine();                          
-                    System.out.println("Thank you for entering a player"); 
+                    background = sc.nextLine();
+                    System.out.println("Thank you for entering a player");
                     try {
                         Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
                         Statement stmt = conn.createStatement();
@@ -139,7 +144,7 @@ public class AADP_Lab_SoccerSimulator {
                         );
                     } catch (SQLException e) {
                         e.printStackTrace();
-                    }      
+                    }
                 } else if (option == 2) {
                     boolean validTeam = false;
                     String teamName;
@@ -153,8 +158,12 @@ public class AADP_Lab_SoccerSimulator {
                                 break;
                             }
                         }
-                        if (teamName.toLowerCase().equals("exit")) break;
-                        if (!validTeam) System.out.println("That is not one of the teams. Please try again!");
+                        if (teamName.toLowerCase().equals("exit")) {
+                            break;
+                        }
+                        if (!validTeam) {
+                            System.out.println("That is not one of the teams. Please try again!");
+                        }
                     } while (!validTeam);
                     try {
                         Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
@@ -172,24 +181,26 @@ public class AADP_Lab_SoccerSimulator {
                             birth = rs.getString("birth");
                             position = rs.getString("position");
                             goalsScored = rs.getInt("goalsScored");
-                            background = rs.getString("background");              
+                            background = rs.getString("background");
                             System.out.println(String.format("Name: %s -- Number: %d -- DoB: %s -- Position: %s -- Number of goals scored: %d", name, number, birth, position, goalsScored));
                             System.out.println("Background:");
                             System.out.println(background);
                         }
                     } catch (SQLException e) {
                         e.printStackTrace();
-                    }                     
+                    }
                 } else if (option == 3) {
                     System.out.println("How many matches would you like to simulate?");
                     Boolean validNum = false;
                     int numMatches = 0;
                     do {
                         try {
-                            numMatches = Integer.parseInt(sc.nextLine()); 
+                            numMatches = Integer.parseInt(sc.nextLine());
                             if (numMatches < 1) {
                                 System.out.println("Please enter a positive integer");
-                            } else validNum = true;
+                            } else {
+                                validNum = true;
+                            }
 
                         } catch (Exception e) {
                             System.out.println("That is not a number. please try again!");
@@ -211,7 +222,7 @@ public class AADP_Lab_SoccerSimulator {
                         } else if (team1Score < team2Score) {
                             System.out.println(String.format("Congratulation %s! %s scored %d goals and %s scored %d goals.", team2, team1, team1Score, team2, team2Score));
                         } else {
-                            System.out.println(String.format("It was a draw!! %s scored %d goals and %s scored %d goals.",  team1, team1Score, team2, team2Score));
+                            System.out.println(String.format("It was a draw!! %s scored %d goals and %s scored %d goals.", team1, team1Score, team2, team2Score));
                         }
                     }
                 } else if (option == 4) {
@@ -222,7 +233,7 @@ public class AADP_Lab_SoccerSimulator {
                 }
             } catch (Exception e) {
                 System.out.println("That is not a number. Please try again!");
-            }         
-        } while (!exit);   
-    }     
+            }
+        } while (!exit);
+    }
 }
--- src/aadp_lab_soccersimulator/Database.java
+++ src/aadp_lab_soccersimulator/Database.java
@@ -4,10 +4,42 @@
  */
 package aadp_lab_soccersimulator;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author rayen
  */
 public class Database {
-    
+
+    protected static final String DB_URL = "jdbc:mysql://localhost/world_cup";
+    protected static final String USER = "football";
+    protected static final String PASS = "Java is almost as good as football";
+
+    //   Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+    public static void setupDatabase(String[] teams) {
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS); Statement stmt = conn.createStatement()) {
+            stmt.execute("CREATE SCHEMA IF NOT EXISTS world_cup;");
+            stmt.execute("USE world_cup;");
+
+            for (String team : teams) {
+                String sql = "CREATE TABLE IF NOT EXISTS " + team + " ("
+                        + "name VARCHAR(30) NOT NULL,"
+                        + "number INT NOT NULL PRIMARY KEY,"
+                        + "birth VARCHAR(30),"
+                        + "position VARCHAR(30),"
+                        + "goalsScored INT,"
+                        + "background TEXT(1000));";
+                stmt.execute(sql);
+            }
+            System.out.println("Tables setup completed successfully in world_cup database...");
+        } catch (SQLException e) {
+            System.out.println("Problem with setting up world_cup database tables:");
+            e.printStackTrace();
+        }
+    }
+
 }
