4a7d2f402b94b7274ea3177248e34c4a0323f846
Rayenbt11 <100359918+Rayenbt11@users.noreply.github.com>
2023-12-15 13:40:21 +0000

    team class

--- src/aadp_lab_soccersimulator/Team.java
+++ src/aadp_lab_soccersimulator/Team.java
@@ -4,10 +4,34 @@
  */
 package aadp_lab_soccersimulator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author rayen
  */
 public class Team {
+    private String name;
+    private List<Player> players;
+
+    public Team(String name) {
+        this.name = name;
+        this.players = new ArrayList<>();
+    }
+
+    // Add a player to the team
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    // Get all players in the team
+    public List<Player> getPlayers() {
+        return players;
+}
+
+    public String getName() {
+        return name;
+    }
     
 }
